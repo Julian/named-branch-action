6e98ac93737d328af283be7566d6d5232cd76691
Julian Berman <Julian@GrayVines.com>
2021-08-03 10:59:52 +0100

    Inline composited again, it's easier.

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -l
-GIT_AUTHOR_NAME='Named Branches GitHub Action'
-GIT_COMMITTER_NAME='Named Branches GitHub Action'
-git notes --ref "$3" add -m "$2"
-git remote set-url origin "$1"
-git push origin "$3:$3"
